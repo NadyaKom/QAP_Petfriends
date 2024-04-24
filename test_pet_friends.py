6581fed6ec9a9ef6b21f3829e46aee8e236f7de4
NadyaKom <157776401+NadyaKom@users.noreply.github.com>
2024-04-24 10:46:10 +0700

    Update test_pet_friends.py

--- test_pet_friends.py
+++ test_pet_friends.py
@@ -18,10 +18,10 @@ def test_get_api_key_for_invalid_email_valid_password(email=invalid_email, passw
      """Проверяем возможность входа при неверном логине и верном пароле """
 
      # Запрашиваем ключ api и сохраняем в переменую auth_key
-      _, auth_key = pf.get_api_key(invalid_email, valid_password)
+    _, auth_key = pf.get_api_key(invalid_email, valid_password)
 
-    assert status == 400
-    assert status == 400
+     assert status == 400
+     assert status == 400
      
 
 def test_get_api_key_for_invalid_email_valid_password(email=valid_email, password=invalid_password):
@@ -37,7 +37,7 @@ def test_get_api_key_for_invalid_user(email=valid_email, password=valid_password
     """Проверяем возможность входа при верном логине и верном пароле """
 
     # Запрашиваем ключ api и сохраняем в переменую auth_key
-     _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
     
     assert status == 200
     assert 'key' in result
@@ -67,7 +67,7 @@ def test_add_new_pet_with_invalid_data_invalid_animal_type(name='Персик', 
     # Добавляем питомца
     status, result = pf.add_new_pet(auth_key, name, animal_type, age)
     
-   assert status == 200
+    assert status == 200
     assert result['name'] == name
                                     
 def test_add_new_pet_with_incomplete_data(name='Персик', animal_type=' ',
@@ -93,8 +93,8 @@ def test_get_my_pets_with_valid_key(my_pets):
     assert status == 200
     assert len(result['pets']) == my_pets
 
-def test_delete_pet_by_id(pet_id='f70212c3-4ced-4fa7-b4a5-17041a6f8454')
-   """ Проверяем удаление питомца по id""" 
+def test_delete_pet_by_id(pet_id='f70212c3-4ced-4fa7-b4a5-17041a6f8454'):
+    """ Проверяем удаление питомца по id""" 
    
     pet_id = 'f3043661-b5a7-41b2-be7c-39e1e3141290'
     status, _ = pf.delete_pet(auth_key, pet_id)
